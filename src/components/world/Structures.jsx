b30f5c4e1eea8ffbe3c2033f05faa432732450f8
stux <hello@mstdn.social>
2023-11-30 23:39:31 +0100

    Scene elements

--- src/components/world/Structures.jsx
+++ src/components/world/Structures.jsx
@@ -58,54 +58,71 @@ const Iglo = (props) =>
 /**
  * Cannon: https://poly.pizza/m/Eko3cjAMW9
  */
-// const Cannon = (props) => 
-// {
-//     const body = useRef()
-//     const { char, pos } = props
-//     const [ fired, setFired ] = useState(false)
-//     const { nodes, materials } = useGLTF("./assets/models/world/cannon.glb")
-//     const [ playCannonSound ] = useSound('./assets/audio/cannon.wav', { volume: 1, interrupt: true })
+const Gifts = (props) => 
+{
+    const body = useRef()
+    const { char, pos } = props
+    const [ fired, setFired ] = useState(false)
+    const { nodes, materials } = useGLTF("./assets/models/world/gifts.glb")
+    // const [ playCannonSound ] = useSound('./assets/audio/cannon.wav', { volume: 1, interrupt: true })
 
-//     useFrame(() =>
-//     {
-//         if(char.current)
-//         {
-//             const position = new Vector3(pos[0], pos[1], pos[2])
-//             const charPosition = char.current.translation()
-//             const distance = position.distanceTo(new Vector3(charPosition.x, charPosition.y, charPosition.z))
+    useFrame(() =>
+    {
+        if(char.current)
+        {
+            const position = new Vector3(pos[0], pos[1], pos[2])
+            const charPosition = char.current.translation()
+            const distance = position.distanceTo(new Vector3(charPosition.x, charPosition.y, charPosition.z))
             
-//             if(distance < 8 && !fired)
-//             {
-//                 playCannonSound()
-//                 body.current.applyImpulse( { x: 0, y: 300, z: 0 } )
-//                 setFired(true)
-//             } 
-//             else if(distance > 8 && fired)
-//             {
-//                 setFired(false)
-//             }
-//         }
-//     })
+            if(distance < 8 && !fired)
+            {
+                // playCannonSound()
+                body.current.applyImpulse( { x: 0, y: 100, z: 0 } )
+                setFired(true)
+            } 
+            else if(distance > 8 && fired)
+            {
+                setFired(false)
+            }
+        }
+    })
 
-//     return (
-//         <RigidBody
-//             ref={ body }
-//             gravityScale={ 0.9 }
-//             friction={ 0.2 }
-//             restitution={ 0.2 }
-//         >
-//             <group {...props} dispose={null}>
-//                 <mesh
-//                     castShadow
-//                     receiveShadow
-//                     geometry={nodes.Prop_Cannon.geometry}
-//                     material={materials.Atlas}
-//                     scale={100}
-//                 />
-//             </group>
-//         </RigidBody>
-//     )
-// }
+    return (
+        <RigidBody
+            ref={ body }
+            gravityScale={ 1 }
+            friction={ 0.2 }
+            restitution={ 0.1 }
+        >
+            <group {...props} dispose={null}>
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes["Node-Mesh"].geometry}
+                    material={materials.mat8}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes["Node-Mesh_1"].geometry}
+                    material={materials.mat9}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes["Node-Mesh_2"].geometry}
+                    material={materials.mat21}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes["Node-Mesh_3"].geometry}
+                    material={materials.mat12}
+                />
+            </group>
+        </RigidBody>
+    )
+}
 
 
 
@@ -123,8 +140,17 @@ export default function Structures(props)
                 rotation-y={ Math.PI * 1 }
                 rotation-z={ - Math.PI * 0.03 }
             />
+
+            <Gifts 
+                position={ [ - 6.4, - 5, - 26.7 ] } 
+                pos={ [ - 6.4, - 5, - 26.7 ] } 
+                scale={ 1 } 
+                rotation-y={ Math.PI * 0.25 } 
+                char={ char } 
+            />
         </>
     )
 }
 
 useGLTF.preload("./assets/models/world/iglo.glb")
+useGLTF.preload("./assets/models/world/gifts.glb")
--- src/components/world/Trees.jsx
+++ src/components/world/Trees.jsx
@@ -31,10 +31,10 @@ const Tree1 = (props) =>
 {
     const { nodes, materials } = useGLTF("./assets/models/world/trees/tree-1.glb")
     return (
-        <RigidBody
-            type="fixed"
-            colliders="trimesh"
-        >
+        // <RigidBody
+        //     type="fixed"
+        //     // colliders="hull"
+        // >
             <group {...props} dispose={null}>
                 <mesh
                     castShadow
@@ -49,27 +49,309 @@ const Tree1 = (props) =>
                     material={materials.mat20}
                 />
             </group>
-        </RigidBody>
+        // </RigidBody>
     )
 }
 
 /**
- * House 
+ * Tree 1 
  */
-// const HouseX = (props) => 
-// {
-//     const { nodes, materials } = useGLTF("./assets/models/world/trees/palm-.glb")
-//     return (
-//         <RigidBody
-//             type="fixed"
-//             colliders="trimesh"
-//         >
-//             <group {...props} dispose={null}>
-               
-//             </group>
-//         </RigidBody>
-//     )
-// }
+const PineTrees = (props) => 
+{
+    const { nodes, materials } = useGLTF("./assets/models/world/trees/pine-tree.glb")
+    return (
+        // <RigidBody
+        //     type="fixed"
+        //     // colliders="hull"
+        // >
+            <group {...props} dispose={null}>
+                <group scale={ 0.05 }>
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={nodes.pine_tree_1__001_1.geometry}
+                        material={materials["03___Default"]}
+                    />
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={nodes.pine_tree_1__001_1_1.geometry}
+                        material={materials.Material}
+                    />
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={nodes.pine_tree_1__001_1_2.geometry}
+                        material={materials["02___Default"]}
+                    />
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={nodes.pine_tree_1_var_1_1.geometry}
+                        material={materials["03___Default"]}
+                    />
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={nodes.pine_tree_1_var_1_1_1.geometry}
+                        material={materials["02___Default"]}
+                    />
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={nodes.pine_tree_1__1.geometry}
+                        material={materials["03___Default"]}
+                    />
+                    <mesh
+                        castShadow
+                        receiveShadow
+                        geometry={nodes.pine_tree_1__1_1.geometry}
+                        material={materials["02___Default"]}
+                    />
+                </group>
+            </group>
+        // </RigidBody>
+    )
+}
+
+/**
+ * ChristmasTree 
+ */
+const ChristmasTree = (props) => 
+{
+    const { nodes, materials } = useGLTF("./assets/models/world/trees/christmas-tree.glb")
+    return (
+        <RigidBody
+            type="fixed"
+            // colliders="hull"
+        >
+            <group {...props} dispose={null}>
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group1089073295.geometry}
+                    material={materials.mat8}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group314590998.geometry}
+                    material={materials.mat12}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group214022184.geometry}
+                    material={materials.mat2}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group922588407.geometry}
+                    material={materials.mat12}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group1410509470.geometry}
+                    material={materials.mat2}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group179461400.geometry}
+                    material={materials.mat12}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group1832615123.geometry}
+                    material={materials.mat8}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group411241951.geometry}
+                    material={materials.mat8}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group1633066821.geometry}
+                    material={materials.mat8}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group1164989660.geometry}
+                    material={materials.mat8}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group1127207942.geometry}
+                    material={materials.mat12}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group2051798186.geometry}
+                    material={materials.mat2}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group1084830891.geometry}
+                    material={materials.mat12}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group75024073.geometry}
+                    material={materials.mat12}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group615263083.geometry}
+                    material={materials.mat10}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group148092846.geometry}
+                    material={materials.mat10}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group667748054.geometry}
+                    material={materials.mat10}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group558281359.geometry}
+                    material={materials.mat10}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group1540402439.geometry}
+                    material={materials.mat10}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group1263511113.geometry}
+                    material={materials.mat20}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.group1827880687.geometry}
+                    material={materials.mat12}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.mesh1781168731.geometry}
+                    material={materials.mat21}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.mesh1781168731_1.geometry}
+                    material={materials.mat8}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.mesh948351503.geometry}
+                    material={materials.mat8}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.mesh948351503_1.geometry}
+                    material={materials.mat21}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.mesh792452036.geometry}
+                    material={materials.mat8}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.mesh792452036_1.geometry}
+                    material={materials.mat21}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.mesh814621041.geometry}
+                    material={materials.mat8}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.mesh814621041_1.geometry}
+                    material={materials.mat21}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.mesh1969426104.geometry}
+                    material={materials.mat8}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.mesh1969426104_1.geometry}
+                    material={materials.mat21}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.mesh77345095.geometry}
+                    material={materials.mat8}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.mesh77345095_1.geometry}
+                    material={materials.mat21}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.mesh1157521227.geometry}
+                    material={materials.mat8}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.mesh1157521227_1.geometry}
+                    material={materials.mat21}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.mesh662924655.geometry}
+                    material={materials.mat8}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.mesh662924655_1.geometry}
+                    material={materials.mat21}
+                />
+            </group>
+        </RigidBody>
+    )
+}
 
 
 
@@ -78,8 +360,29 @@ export default function Trees()
     return(
         <>
             <Tree1 position={ [ - 27, - 4.5, - 9 ] } scale={ 10 } rotation={ [ 0, Math.PI * 0.5, 0 ] } />
+            <Tree1 position={ [ 13, - 1.2, - 45 ] } scale={ 8 } rotation={ [ 0, Math.PI * 1, 0 ] } />
+            <Tree1 position={ [ 28, 0.9, - 28 ] } scale={ 9 } rotation={ [ 0, Math.PI * 0.2, 0 ] } />
+            <Tree1 position={ [ 30, 2, 0 ] } scale={ 10 } rotation={ [ 0, Math.PI * 0.2, 0 ] } />
+            <Tree1 position={ [ 3.2, - 0.2, 22 ] } scale={ 9 } rotation={ [ 0, Math.PI * 0.2, 0 ] } />
+            <Tree1 position={ [ - 27, - 3.3, 20 ] } scale={ 10 } rotation={ [ 0, Math.PI * 0.2, 0 ] } />
+            
+            <PineTrees 
+                position={ [ - 29, - 9.3, - 38 ] } 
+                scale={ 0.8 } 
+                rotation={ [ - Math.PI * 0, Math.PI * 0.9, - Math.PI * 0.05 ] } 
+            />
+            <PineTrees 
+                position={ [ 46, - 1.5, - 69 ] } 
+                scale={ 0.8 } 
+                rotation={ [ - Math.PI * 0.04, Math.PI * 2.2, Math.PI * 0.03 ] } 
+            />
+            
+            
+            <ChristmasTree position={ [ 15, 1, - 16 ] } scale={ 8 } rotation={ [ 0, Math.PI * 0.5, 0 ] } />
         </>
     )
 }
 
 useGLTF.preload("./assets/models/world/trees/tree-1.glb")
+useGLTF.preload("./assets/models/world/trees/pine-tree.glb")
+useGLTF.preload("./assets/models/world/trees/christmas-tree.glb")
